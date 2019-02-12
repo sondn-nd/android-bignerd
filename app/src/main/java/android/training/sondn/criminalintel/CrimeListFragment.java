6ca53b38416cc604ffcd856646bf91fe6f8c773b
sondn <sondn-nd@hotmail.com>
2019-02-12 22:45:13 +0700

    C11 Using ViewPager

--- app/src/main/java/android/training/sondn/criminalintel/CrimeListFragment.java
+++ app/src/main/java/android/training/sondn/criminalintel/CrimeListFragment.java
@@ -66,7 +66,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
-            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
             startActivity(intent);
         }
     }
